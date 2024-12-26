cfdc942efbeb28896433e86435e6794df0befb43
JEONTAK <tak980418@naver.com>
2024-12-26 15:43:55 +0900

    Edit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 //main text 타이핑 효과
-if (window.location.pathname.endsWith("index.html")) {
+if (window.location.pathname === "/" ||
+    window.location.pathname.endsWith("index.html")) {
     const $txt = document.querySelector(".txt-title");
     const content = "Hello!!\nMy Name Is Jeon Tak :)";
     let contentIndex = 0;
@@ -18,3 +19,12 @@ if (window.location.pathname.endsWith("index.html")) {
 
     const typingInterval = setInterval(typing, 200);
 }
+
+
+const path = window.location.pathname;
+// URL에서 `~~~.html`이 포함된 경우 제거
+if (path.endsWith("html")) {
+    const newPath = path.replace(".html", "");
+    window.history.replaceState(null, "", newPath);
+    console.log(newPath);
+  }
